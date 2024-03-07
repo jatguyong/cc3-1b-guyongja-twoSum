5a1e310b9bce05c879a3b1245153c8a57e48e2a7
JENNY ANN GUYONG <jtg6745@students.uc-bcf.edu.ph>
2024-03-07 22:18:03 +0800

    Add TwoSum cs project

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum {
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] numArray = {2, 5, 7, 11, 4};
+            int target = 9;
+            int[] result = TwoSum(numArray, target);
+            Console.WriteLine("Output: [" + string.Join(", ", result) + "]");
+        }
+
+        public static int[] TwoSum (int[] numArray, int target)
+        {
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+            int number = numArray[i];
+            int targetNumber = target - number;
+            
+            // if the targetNumber exists in the dictionary
+            if (numDict.ContainsKey(targetNumber)) {
+                // return an integer array containing the index of the targetNumber and the current 'number'
+                return new int[] { numDict[targetNumber], i };
+            }
+
+            // only store the index of the first occurence of the number with duplicates
+            if (!numDict.ContainsKey(number))
+                numDict[number] = i;
+            }
+
+            // else return empty array
+            return new int[0];
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integers separated by commas: ");
+            string numInput = Console.ReadLine();
+            int[] numArray = Array.ConvertAll(numInput.Split(','), int.Parse);
+
+            Console.Write("Enter the target sum: ");
+            int target = int.Parse(Console.ReadLine());
+
+            int[] result = TwoSum(numArray, target);
+            Console.WriteLine("Output: [" + string.Join(", ", result) + "]");
+        }
+
+        static int[] TwoSum(int[] numArray, int target)
+        {
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                int number = numArray[i];
+                int targetNumber = target - number;
+
+                // if the targetNumber exists in the dictionary
+                if (numDict.ContainsKey(targetNumber))
+                {
+                    // return an integer array containing the index of the targetNumber and the current 'number'
+                    return new int[] { numDict[targetNumber], i };
+                }
+
+                // only store the index of the first occurence of the number with duplicates
+                if (!numDict.ContainsKey(number))
+                    numDict[number] = i;
+            }
+
+            // else return an empty array
+            return new int[0];
+        }
+    }
+}
